613993cfb1ea6ce49ad0ce36ebf9063a1bf19da1
aXayitov <hayitovabdurahmon76@gmail.com>
2024-11-25 09:31:49 +0500

    ..

---
+++ BarbershopManagementSystem.WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,15 @@
+﻿using BarbershopManagementSystem.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarbershopManagementSystem.WebUI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index(SettingViewModel setting)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ BarbershopManagementSystem.WebUI/ViewModels/SettingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BarbershopManagementSystem.WebUI.ViewModels
+{
+    public class SettingViewModel
+    {
+        public string PhoneNumber { get; set; }
+        public string Title { get; set; }
+    }
+}
